9285cc5f7ce6afa0738f0e42ecaf2eaccdfdb53a
Angela <98893476+amyers0217@users.noreply.github.com>
2023-03-16 20:36:00 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
+import { FilterPipe } from './filter.pipe';
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContentCardComponent,
+    ContentListComponent,
+    FilterPipe
+  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
