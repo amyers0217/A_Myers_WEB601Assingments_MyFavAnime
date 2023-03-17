51c4677cc237619b769dbd7624519c8f709dc7fb
Angela <98893476+amyers0217@users.noreply.github.com>
2023-03-16 20:48:49 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
+import { FilterPipe } from './filter.pipe';
+import {FormsModule} from "@angular/forms";
+import { HoverAffectDirective } from './hover-affect.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContentCardComponent,
+    ContentListComponent,
+    FilterPipe,
+    HoverAffectDirective
+  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
